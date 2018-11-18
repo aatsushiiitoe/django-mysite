886ace48a98ec8c7cc453837f3ccb915c9f9e850
kuragen <newfrog98@gmail.com>
2018-11-18 12:34:04 +0700

    テンプレート追加

--- manager_project/mysite/polls/views.py
+++ manager_project/mysite/polls/views.py
@@ -5,13 +5,6 @@ from django.views import generic
 
 from .models import Choice, Question
 
-from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
-from django.urls import reverse
-from django.views import generic
-
-from .models import Choice, Question
-
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
