32303f3dd7c46b6e5eb7d9326055246598f30d05
kuragen <newfrog98@gmail.com>
2018-11-21 21:55:02 +0700

    todo追加

---
+++ manager_project/mysite/todo/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from .models import Post
+
+#レコード表示機能
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('message','created_date',)
+    list_display_links = ('message','created_date',)
+    list_filter = ['created_date']
+    
+admin.site.register(Post,PostAdmin)
+# Register your models here.
