adaf036b299371ead37c7956289f6936aa28ada9
kuragen <newfrog98@gmail.com>
2018-12-12 00:06:06 +0700

    todo+2

--- manager_project/mysite/todo/admin.py
+++ manager_project/mysite/todo/admin.py
@@ -1,12 +1,24 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post,Todo,Detail
 
 #レコード表示機能
 class PostAdmin(admin.ModelAdmin):
+    
     list_display = ('message','link','created_date',)
     list_display_links = ('message','link','created_date',)
     list_filter = ['created_date']
+
     
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('todo_text', 'pub_date')
+    
+class DetailInline(admin.TabularInline):
+    model = Detail
+    extra = 3
+                    
 admin.site.register(Post,PostAdmin)
+admin.site.register(Todo,TodoAdmin)
+admin.site.register(Detail)
+
 # Register your models here.
--- manager_project/mysite/todo/models.py
+++ manager_project/mysite/todo/models.py
@@ -14,3 +14,21 @@ class Post(models.Model):
         auto_now_add=True,
         verbose_name="登録日時",
         )
+
+
+class Todo(models.Model):
+    todo_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+    
+class Detail(models.Model):
+    todo = models.ForeignKey(Todo, on_delete=models.CASCADE)
+    detail_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.detail_text
