4ffbbd5efe15cb96a24ee1ec15324e73ed332c2f
kuragen <newfrog98@gmail.com>
2018-12-11 22:02:00 +0700

    todo +1

--- manager_project/mysite/todo/admin.py
+++ manager_project/mysite/todo/admin.py
@@ -4,8 +4,8 @@ from .models import Post
 
 #レコード表示機能
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('message','created_date',)
-    list_display_links = ('message','created_date',)
+    list_display = ('message','link','created_date',)
+    list_display_links = ('message','link','created_date',)
     list_filter = ['created_date']
     
 admin.site.register(Post,PostAdmin)
